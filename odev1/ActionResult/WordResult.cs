df6edef5460cc5d0a2db0777edfc1c34c8764971
user <user@DESKTOP-30MLNJT>
2021-04-13 15:11:37 +0300

    Response Header kullanımı

---
+++ odev1/ActionResult/WordResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Abis.Web.Core.ActionResults
+{
+    public class WordResult : ActionResult
+    {
+        public WordResult(string viewName)
+        {
+            ViewName = viewName;
+        }
+
+        public string FileName { get; set; }
+        public string ViewName { get; set; }
+        public object Model { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.Controller.ViewData.Model = Model;
+
+            using (var stream = new MemoryStream())
+            using (var writer = new StreamWriter(stream))
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(context, ViewName);
+                var viewContext = new ViewContext(context, viewResult.View, context.Controller.ViewData, context.Controller.TempData, writer);
+                viewResult.View.Render(viewContext, writer);
+                viewResult.ViewEngine.ReleaseView(context, viewResult.View);
+                writer.Flush();
+
+                context.HttpContext.Response.Clear();
+                context.HttpContext.Response.BinaryWrite(stream.ToArray());
+                context.HttpContext.Response.ContentType = "application/msword";
+                context.HttpContext.Response.AddHeader("content-disposition", $"filename={FileName}.doc");
+                context.HttpContext.Response.End();
+            }
+        }
+    }
+}
---
+++ odev1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Abis.Web.Core.ActionResults;
+using odev1.Models;
+using OfficeOpenXml;
+using Rotativa;
+using System.Web.Mvc;
+
+namespace odev1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Download(StudentModel model)
+        {
+            if (model.DownloadType == 1)
+            {
+                return new WordResult("Download") { FileName = $"{model.No.ToString()}_{model.Name}_{model.Surname}", Model = model };
+            }
+            if (model.DownloadType == 2)
+            {
+                return new ViewAsPdf("Download", model);
+            }
+            if (model.DownloadType == 3)
+            {
+                ExcelPackage.LicenseContext = LicenseContext.Commercial;
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (var excel = new ExcelPackage())
+                {
+                    var workSheet = excel.Workbook.Worksheets.Add($"{model.No.ToString()}_{model.Name}_{model.Surname}");
+                    workSheet.Row(1).Style.Font.Bold = true;
+                    workSheet.Cells[1, 1].Value = "Öğrenci No";
+                    workSheet.Cells[1, 2].Value = "Öğrenci Adı";
+                    workSheet.Cells[1, 3].Value = "Öğrenci Soyadı";
+                    int recordIndex = 2;
+                    workSheet.Cells[recordIndex, 1].Value = model.No;
+                    workSheet.Cells[recordIndex, 2].Value = model.Name;
+                    workSheet.Cells[recordIndex, 3].Value = model.Surname;
+                    recordIndex++;
+                    workSheet.Cells.AutoFitColumns();
+                    var result = excel.GetAsByteArray();
+                    var fileName = $"{model.No.ToString()}_{model.Name}_{model.Surname}.xlsx";
+                    return File(result, "application/vnd.ms-excel", fileName);
+                }
+            }
+            return View(model);
+        }
+    }
+}
